079393fb58e757e7b1cb2170c882d7d7b9a0cbb0
Dobrobatko Dmytro <dobrobatka@gmail.com>
2023-09-08 13:24:41 +0300

    add update

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,11 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
+union = 2;
+union = "string";
+
+type EnableOrDisable = "enable" | "disable";
+let value: EnableOrDisable;
+value = "enable";
+value = "disable";
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,55 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayOfWeek {
+  firstDay = "MONDAY",
+  secondDay = "THUESDAY",
+  thirdDay = "WEDNESDAY",
+  fourthDay = "THURSDAY",
+  fifthDay = "FRIDAY",
+  sixthDay = "SATURDAY",
+  seventhDay = "SUNDAY",
+}
+
+const isWeekend = (day: DayOfWeek): boolean => {
+  let result: boolean;
+
+  switch (day) {
+    case "MONDAY":
+    case "THUESDAY":
+    case "WEDNESDAY":
+    case "THURSDAY":
+    case "FRIDAY":
+      result = true;
+      break;
+    default:
+      result = false;
+  }
+
+  return result;
+};
+
+console.log(isWeekend(DayOfWeek.seventhDay));
+console.log(isWeekend(DayOfWeek.firstDay));
+
+// let isWeekend: (day: DayOfWeek) => boolean;
+
+// isWeekend = (day: string) => {
+//   let result: boolean;
+//   switch (day) {
+//     case "MONDAY":
+//     case "THUESDAY":
+//     case "WEDNESDAY":
+//     case "THURSDAY":
+//     case "FRIDAY":
+//       result = true;
+//       break;
+//     default:
+//       result = false;
+//   }
+
+//   return result;
+// };
+
+// console.log(isWeekend(DayOfWeek.seventhDay));
